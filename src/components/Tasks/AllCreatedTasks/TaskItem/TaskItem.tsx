62d4ae99db1f9745af368c519a923ff326c05377
Martun <martun@xsoft.am>
2022-07-08 13:52:57 +0400

    feat: add mui buttons

--- src/components/Tasks/AllCreatedTasks/TaskItem/TaskItem.tsx
+++ src/components/Tasks/AllCreatedTasks/TaskItem/TaskItem.tsx
@@ -1,11 +1,25 @@
 import { FC } from "react";
 import { ITaskItemProps } from "../../model";
 import css from "./TaskItem.module.css";
+import DoneIcon from "@mui/icons-material/Done";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const TaskItem: FC<ITaskItemProps> = ({ text, id, onRemoveTask }) => {
   return (
-    <li onClick={() => onRemoveTask(id)} className={css.item}>
+    <li className={css.item}>
       {text}
+      <div className={css.btns}>
+        <button className={css.done}>
+          <DoneIcon />
+        </button>
+        <button className={css.edit}>
+          <EditIcon />
+        </button>
+        <button onClick={() => onRemoveTask(id)} className={css.delete}>
+          <DeleteIcon />
+        </button>
+      </div>
     </li>
   );
 };
